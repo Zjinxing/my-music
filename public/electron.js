01f577f2cdc8a149b1676ee3e47ced394d7bbd40
Zjinxing <1182999456@qq.com>
2020-05-20 08:54:03 +0800

    init

---
+++ public/electron.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+const path = require('path');
+
+let win;
+
+const URL =
+  process.env.NODE_ENV === 'dev'
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../build/index.html')}`;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1008,
+    minWidth: 1008,
+    height: 695,
+    minHeight: 695,
+    titleBarStyle: 'hidden',
+    webPreferences: {
+      webSecurity: false,
+      nodeIntegration: true,
+      webviewTag: true,
+    },
+  });
+  win.loadURL(URL);
+  win.webContents.openDevTools();
+  win.on('closed', () => (win = null));
+}
+
+ipcMain.on('maximize', () => {
+  if (win.isMaximized()) {
+    win.unmaximize();
+  } else {
+    win.maximize();
+  }
+});
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
