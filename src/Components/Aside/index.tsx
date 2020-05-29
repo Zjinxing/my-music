a380b190fc5db8c7189445f1af12f188a1126698
Zjinxing <1182999456@qq.com>
2020-05-29 23:35:47 +0800

    header 添加可拖拽区

--- src/Components/Aside/index.tsx
+++ src/Components/Aside/index.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import './index.scss'
 
 const Aside: React.FC = () => {
-  return <div className="aside">Aside</div>
+  return (
+    <div className="aside">
+      <div className="aside-header drag">可拖拽区域</div>
+    </div>
+  )
 }
 
 export default Aside
--- src/Components/Container/index.tsx
+++ src/Components/Container/index.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import './index.scss'
 
 const Container: React.FC = () => {
-  return <div className="container">container</div>
+  return (
+    <div className="container">
+      <div className="container-header drag">可拖拽区域</div>
+    </div>
+  )
 }
 
 export default Container
