97f5b793ffd4334bce538b93c573d7087d6afc30
Zjinxing <1182999456@qq.com>
2020-06-05 08:35:53 +0800

    删除好像没啥用的配置

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-const { override, addWebpackAlias } = require('customize-cra')
-
-module.exports = override(
-  addWebpackAlias({
-    components: path.resolve(__dirname, 'src/components'),
-  })
-)
