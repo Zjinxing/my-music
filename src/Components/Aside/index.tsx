7b2ec28245fe925115dd03663164f933b3e30eef
Zjinxing <1182999456@qq.com>
2020-06-09 23:04:59 +0800

    更改activeClass判断方式

--- src/Components/Aside/index.tsx
+++ src/Components/Aside/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { useObserver } from 'mobx-react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import './index.scss'
 
 const Aside: React.FC = () => {
+  const { pathname } = useLocation()
+  const pathArr = pathname.split('/')
+
   return useObserver(() => (
     <div className="aside">
       <div className="aside-header drag">
@@ -19,17 +22,17 @@ const Aside: React.FC = () => {
         <ul className="aside-nav online">
           <span>在线音乐</span>
           <li className="aside-nav__link">
-            <NavLink exact to="/music" activeClassName="active">
+            <NavLink exact to="/music" className={pathArr.includes('music') ? 'active' : ''}>
               音乐馆
             </NavLink>
           </li>
           <li className="aside-nav__link">
-            <NavLink to="/video" activeClassName="active">
+            <NavLink to="/video" className={pathArr.includes('video') ? 'active' : ''}>
               视频
             </NavLink>
           </li>
           <li className="aside-nav__link">
-            <NavLink to="/radio" activeClassName="active">
+            <NavLink to="/radio" className={pathArr.includes('radio') ? 'active' : ''}>
               电台
             </NavLink>
           </li>
@@ -37,22 +40,22 @@ const Aside: React.FC = () => {
         <ul className="aside-nav local">
           <span>我的音乐</span>
           <li className="aside-nav__link">
-            <NavLink to="/favorite" activeClassName="active">
+            <NavLink to="/favorite" className={pathArr.includes('favorite') ? 'active' : ''}>
               我喜欢
             </NavLink>
           </li>
           <li className="aside-nav__link">
-            <NavLink to="/local" activeClassName="active">
+            <NavLink to="/local" className={pathArr.includes('local') ? 'active' : ''}>
               本地歌曲
             </NavLink>
           </li>
           <li className="aside-nav__link">
-            <NavLink to="/download" activeClassName="active">
+            <NavLink to="/download" className={pathArr.includes('download') ? 'active' : ''}>
               下载歌曲
             </NavLink>
           </li>
           <li className="aside-nav__link">
-            <NavLink to="/playHistory" activeClassName="active">
+            <NavLink to="/playHistory" className={pathArr.includes('playHistory') ? 'active' : ''}>
               播放历史
             </NavLink>
           </li>
