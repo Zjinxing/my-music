3277d9c86b615a2cdd9b2cb85ba7a9eb146635da
Zjinxing <1182999456@qq.com>
2020-06-10 00:37:35 +0800

    添加icon图标

--- src/common/Enum/index.ts
+++ src/common/Enum/index.ts
@@ -1,4 +1,7 @@
 export const imgList = {
+  backAction: require('resources/backAction.png'),
+  nextAction: require('resources/nextAction.png'),
   logo: require('resources/logo.png'),
   logoText: require('resources/logo_text.png'),
+  searchIcon: require('resources/local_search_icon.png'),
 }
