efdb0ac11ac343370bf827e7a96c833789cbd7c0
Zjinxing <1182999456@qq.com>
2020-06-09 23:05:18 +0800

    布局调整

--- src/Components/Container/index.tsx
+++ src/Components/Container/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
-import { useStore } from 'store'
 import { observer } from 'mobx-react'
 import Music from './Music'
 import './index.scss'
@@ -12,19 +11,20 @@ import DownLoad from './Download'
 import PlayHistory from './PlayHistory'
 
 const Container: React.FC = observer(() => {
-  const store = useStore()
-  console.log('........', { store })
   return (
     <div className="container">
       <div className="container-header drag">可拖拽区域</div>
-      <Route path="/music" component={Music}></Route>
-      <Route path="/video" component={Video}></Route>
-      <Route path="/radio" component={Radio}></Route>
-      <Route path="/favorite" component={Favorite}></Route>
-      <Route path="/local" component={Local}></Route>
-      <Route path="/download" component={DownLoad}></Route>
-      <Route path="/playHistory" component={PlayHistory}></Route>
-      <Redirect path="/" to={{ pathname: '/music' }}></Redirect>
+      <div className="container-body">
+        <Route path="/music" component={Music}></Route>
+        <Route path="/video" component={Video}></Route>
+        <Route path="/radio" component={Radio}></Route>
+        <Route path="/favorite" component={Favorite}></Route>
+        <Route path="/local" component={Local}></Route>
+        <Route path="/download" component={DownLoad}></Route>
+        <Route path="/playHistory" component={PlayHistory}></Route>
+        <Redirect path="/" to={{ pathname: '/music' }}></Redirect>
+      </div>
+      <div className="container-footer">底部播放控制</div>
     </div>
   )
 })
