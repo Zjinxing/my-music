2517b83caa791213164d93673935634e30c8e88a
Zjinxing <1182999456@qq.com>
2020-06-15 00:35:41 +0800

    add banner dot

--- src/Components/Container/Music/components/Choiceness/Banner/index.tsx
+++ src/Components/Container/Music/components/Choiceness/Banner/index.tsx
@@ -1,12 +1,46 @@
-import React, { useRef, useState } from 'react'
+import React, { useState } from 'react'
 import './index.scss'
 import { useStore } from 'store'
 
 const Banner: React.FC = () => {
   const store = useStore()
   const [transX, setTransX] = useState(0)
+  const [curDot, setCurDot] = useState(0)
   const focus = store.recommend.focus.data.content
+  const MEDIA_MAX_WIDTH = 1610 // css 媒体查询宽度，超过此宽度显示3张轮播图
   console.log({ focus })
+
+  const scrollRight = () => {
+    const bodyWidth = document.body.getBoundingClientRect().width
+    const visibleImgCount = bodyWidth >= MEDIA_MAX_WIDTH ? 3 : 2
+    const compareNum = (visibleImgCount / focus.length) * 100
+    if (100 + transX === compareNum) {
+      setTransX(0)
+      setCurDot(0)
+    } else if (transX - 2 * compareNum < -100) {
+      setTransX(compareNum - 100)
+      setCurDot(curDot + 1)
+    } else {
+      setTransX(transX - compareNum)
+      setCurDot(curDot + 1)
+    }
+  }
+
+  const scrollLeft = () => {
+    const bodyWidth = document.body.getBoundingClientRect().width
+    const visibleImgCount = bodyWidth >= MEDIA_MAX_WIDTH ? 3 : 2
+    const compareNum = (visibleImgCount / focus.length) * 100
+    if (transX === 0) {
+      setTransX(compareNum - 100)
+      setCurDot(Math.ceil(focus.length / visibleImgCount))
+    } else if (0 - transX < compareNum) {
+      setTransX(0)
+      setCurDot(curDot - 1)
+    } else {
+      setTransX(transX + compareNum)
+      setCurDot(curDot - 1)
+    }
+  }
 
   const imgList = focus.map((item) => (
     <li className="banner-content-list-item" key={item.id}>
@@ -14,34 +48,16 @@ const Banner: React.FC = () => {
     </li>
   ))
 
-  const scrollRight = () => {
+  const BannerDot = () => {
     const bodyWidth = document.body.getBoundingClientRect().width
-    const MEDIA_MAX_WIDTH = 1610 // css 媒体查询宽度，超过此宽度显示3张轮播图
     const visibleImgCount = bodyWidth >= MEDIA_MAX_WIDTH ? 3 : 2
-    const compareNum = (visibleImgCount / focus.length) * 100
-    if (100 + transX === compareNum) {
-      setTransX(0)
-    } else if (transX - 2 * compareNum < -100) {
-      setTransX(compareNum - 100)
-    } else {
-      setTransX(transX - compareNum)
-    }
+    let dotCount = Math.ceil(focus.length / visibleImgCount)
+    const arr: number[] = []
+    while (dotCount--) arr.unshift(dotCount)
+    return arr.map((item) => (
+      <li className={`banner-dot-item ${item === curDot ? 'highlight' : ''}`} key={item}></li>
+    ))
   }
-
-  const scrollLeft = () => {
-    const bodyWidth = document.body.getBoundingClientRect().width
-    const MEDIA_MAX_WIDTH = 1610 // css 媒体查询宽度，超过此宽度显示3张轮播图
-    const visibleImgCount = bodyWidth >= MEDIA_MAX_WIDTH ? 3 : 2
-    const compareNum = (visibleImgCount / focus.length) * 100
-    if (transX === 0) {
-      setTransX(compareNum - 100)
-    } else if (0 - transX < compareNum) {
-      setTransX(0)
-    } else {
-      setTransX(transX + compareNum)
-    }
-  }
-
   return (
     <div className="banner">
       <img
@@ -68,8 +84,8 @@ const Banner: React.FC = () => {
         >
           {imgList}
         </ul>
+        <ul className="banner-dot">{BannerDot()}</ul>
       </div>
-      <div className="banner-dot"></div>
     </div>
   )
 }
