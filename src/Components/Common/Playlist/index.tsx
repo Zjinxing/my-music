b44b0eac78f681fd03c52d85b3b7a0f917cd7e86
Zjinxing <1182999456@qq.com>
2020-06-21 23:47:19 +0800

    官方歌单

--- src/Components/Common/Playlist/index.tsx
+++ src/Components/Common/Playlist/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './index.scss'
+import { useStore } from 'store'
 
 interface PlaylistProps {
   listeners: number
@@ -9,10 +10,27 @@ interface PlaylistProps {
 }
 
 const Playlist: React.FC<PlaylistProps> = props => {
+  const listenCount = props.listeners / 10000
+  const store = useStore()
   return (
     <li className="playlist-cover">
-      <img src={props.picurl} alt="" className="playlist-cover--bg" />
-      <span className="listeners">{props.listeners}</span>
+      <div className="playlist-cover-content">
+        <img src={props.picurl} alt="" className="playlist-cover-content--bg" />
+        <span className="playlist-cover-content--control">
+          <img
+            src={
+              store.currentPlaylistId === props.id
+                ? require('common/Enum').imgList.pause
+                : require('common/Enum').imgList.play
+            }
+            alt=""
+          />
+        </span>
+        <span className="listeners">
+          <img src={require('common/Enum').imgList.listen} alt="" width="16" className="icon" />
+          {listenCount.toFixed(1)}万
+        </span>
+      </div>
       <span>{props.title}</span>
     </li>
   )
--- src/Components/Container/Music/components/Choiceness/QQPlaylist/index.tsx
+++ src/Components/Container/Music/components/Choiceness/QQPlaylist/index.tsx
@@ -6,11 +6,21 @@ import './index.scss'
 const QQPlaylist: React.FC = () => {
   const store = useStore()
   const qqPlaylist = store.recommend.req_2.data.modules[0].grids
+  const List = qqPlaylist.map(item => (
+    <Playlist
+      picurl={item.picurl}
+      id={item.id}
+      title={item.title}
+      listeners={item.listeners}
+      key={item.id}
+    />
+  ))
   return (
     <section className="qq-playlist">
-      <h2>
+      <h2 className="qq-playlist--title">
         官方歌单 <small>官方甄选订阅歌单</small>
       </h2>
+      <ul className="qq-playlist--list">{List}</ul>
     </section>
   )
 }
