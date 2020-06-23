d47c4736e60a62a55647bfc46b291ef4534c64e1
Zjinxing <1182999456@qq.com>
2020-06-23 08:58:19 +0800

    更改文件名

--- src/Components/Common/SwipList/index.tsx
+++ src/Components/Common/SwipList/index.tsx
@@ -1,25 +1,23 @@
 import React from 'react'
-import './index.scss'
 import { useStore } from 'store'
+import { Grid } from 'request/types/Recommend'
+import './index.scss'
 
 interface PlaylistProps {
-  listeners: number
-  picurl: string
-  title: string
-  id: number
+  swipList: Grid[]
+  type: 'qq' | 'hiper' | 'latest'
 }
 
 const Playlist: React.FC<PlaylistProps> = props => {
-  const listenCount = props.listeners / 10000
   const store = useStore()
-  return (
-    <li className="playlist-cover">
+  const liItem = props.swipList.map(item => (
+    <li className="playlist-cover" key={item.id}>
       <div className="playlist-cover-content">
-        <img src={props.picurl} alt="" className="playlist-cover-content--bg" />
+        <img src={item.picurl} alt="" className="playlist-cover-content--bg" />
         <span className="playlist-cover-content--control">
           <img
             src={
-              store.currentPlaylistId === props.id
+              store.currentPlaylistId === item.id
                 ? require('common/Enum').imgList.pause
                 : require('common/Enum').imgList.play
             }
@@ -28,12 +26,18 @@ const Playlist: React.FC<PlaylistProps> = props => {
         </span>
         <span className="listeners">
           <img src={require('common/Enum').imgList.listen} alt="" width="16" className="icon" />
-          {listenCount.toFixed(1)}万
+          {(item.listeners / 10000).toFixed(1)}万
         </span>
       </div>
-      <span>{props.title}</span>
+      <span className="playlist-cover--title">{item.title}</span>
+      {props.type === 'qq' ? (
+        ''
+      ) : (
+        <span className="playlist-cover--creator">{item.creator.nick}</span>
+      )}
     </li>
-  )
+  ))
+  return <ul className="swip_list">{liItem}</ul>
 }
 
 export default Playlist
