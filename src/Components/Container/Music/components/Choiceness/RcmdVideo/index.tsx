e58167b55fce3dd3bd50e00911de168c2ca3396d
Zjinxing <1182999456@qq.com>
2020-06-30 08:54:40 +0800

    推荐视频左右滑动

--- src/Components/Container/Music/components/Choiceness/RcmdVideo/index.tsx
+++ src/Components/Container/Music/components/Choiceness/RcmdVideo/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MvCover from 'components/Common/MvCover'
 import { VideoData } from 'request/types/Recommend'
 import './index.scss'
@@ -8,7 +8,44 @@ interface Props {
 }
 
 const Video: React.FC<Props> = props => {
+  const [transX, setTransX] = useState(0)
   const list = props.mvList.map(mv => <MvCover mvData={mv} key={mv.vid} />)
+  const MEDIA_WIDTH_MEDIUM = 1190
+  const MEDIA_WIDTH_LARGE = 1370
+
+  const scrollLeft = () => {
+    const { length } = list
+    const { width: bodyWidth } = document.body.getBoundingClientRect()
+    const compareNum =
+      bodyWidth <= MEDIA_WIDTH_MEDIUM
+        ? Math.ceil(length / 3) - 1
+        : bodyWidth <= MEDIA_WIDTH_LARGE
+        ? Math.ceil(length / 4) - 1
+        : Math.ceil(length / 5) - 1
+
+    if (transX === 0) {
+      setTransX(-compareNum * 100)
+    } else {
+      setTransX(transX + 100)
+    }
+  }
+
+  const scrollRight = () => {
+    const { length } = list
+    const { width: bodyWidth } = document.body.getBoundingClientRect()
+    const compareNum =
+      bodyWidth <= MEDIA_WIDTH_MEDIUM
+        ? Math.ceil(length / 3) - 1
+        : bodyWidth <= MEDIA_WIDTH_LARGE
+        ? Math.ceil(length / 4) - 1
+        : Math.ceil(length / 5) - 1
+    if (Math.abs(transX / 100) === compareNum) {
+      setTransX(0)
+    } else {
+      setTransX(transX - 100)
+    }
+  }
+
   return (
     <section className="rcmdVideo">
       <div className="rcmdVideo-title">
@@ -21,15 +58,17 @@ const Video: React.FC<Props> = props => {
           alt=""
           width="40"
           className="rcmdVideo-list-control--left"
+          onClick={scrollLeft}
         />
         <img
           src={require('common/Enum').imgList.leftArrow}
           alt=""
           width="40"
           className="rcmdVideo-list-control--right"
+          onClick={scrollRight}
         />
         <div className="rcmdVideo-list">
-          <ul>{list}</ul>
+          <ul style={{ transform: `translateX(${transX}%)` }}>{list}</ul>
         </div>
       </div>
     </section>
