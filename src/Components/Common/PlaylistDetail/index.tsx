49f0cab37aa81f8aba148b12eaa1225c2141de16
Zjinxing <1182999456@qq.com>
2020-08-08 23:37:40 +0800

    add playlist detail

--- src/Components/Common/PlaylistDetail/index.tsx
+++ src/Components/Common/PlaylistDetail/index.tsx
@@ -3,6 +3,9 @@ import { RouteComponentProps } from 'react-router-dom'
 import { GET_LIST_DETAIL } from 'request/playlist'
 import { Cdlist } from 'request/types/Playlist'
 import Button from 'components/Common/Button'
+import { Tabs } from 'antd'
+import 'antd/lib/tabs/style/index.css'
+import { formatSeconds } from 'common/utils'
 import './index.scss'
 
 interface RouteProps {
@@ -20,6 +23,7 @@ const PlaylistDetail: React.FC<RouteComponentProps<RouteProps>> = props => {
   }, [props.match.params.id])
 
   console.log('ID', props.match.params.id)
+  const { TabPane } = Tabs
   return (
     <div className="playlist">
       <div className="playlist-header">
@@ -30,7 +34,9 @@ const PlaylistDetail: React.FC<RouteComponentProps<RouteProps>> = props => {
             <img src={listDetail?.headurl} alt="" width="24" className="header-info--avatar" />
             <span className="header-info--nick">{listDetail?.nickname}</span>
             {listDetail?.tags.map(tag => (
-              <span className="header-info--tags">#{tag.name}</span>
+              <span className="header-info--tags" key={tag.id}>
+                #{tag.name}
+              </span>
             ))}
           </p>
           <p className="header-desc one-line-ellipsis">{listDetail?.desc}</p>
@@ -49,7 +55,79 @@ const PlaylistDetail: React.FC<RouteComponentProps<RouteProps>> = props => {
           </div>
         </div>
       </div>
-      <div className="playlist-content">CONTENT</div>
+      <div className="playlist-content">
+        <Tabs defaultActiveKey="song">
+          <TabPane tab={`歌曲${listDetail?.songlist.length || ''}`} key="song">
+            <ul className="songlist">
+              {listDetail?.songlist.map(song => (
+                <li className="songlist-item">
+                  <div className="songlist-item--name one-line-ellipsis">
+                    <span className="songlist-item--name__text one-line-ellipsis">
+                      <span className="icon-wrapper"></span>
+                      {song.name}
+                    </span>
+                    <div className="songlist-item--icons">
+                      {song.isonly ? (
+                        <img src={require('common/Enum').imgList.listVIPICon} alt="" />
+                      ) : null}
+                      {song.file.size_ape || song.file.size_flac ? (
+                        <img src={require('common/Enum').imgList.listSQICon} alt="" />
+                      ) : null}
+                      {song.mv.id ? (
+                        <img
+                          src={require('common/Enum').imgList.listMVICon}
+                          className="mv-icon"
+                          alt=""
+                        />
+                      ) : (
+                        ''
+                      )}
+                    </div>
+                    <div className="songlist-item--controls">
+                      <span className="controls-icon">
+                        <img
+                          src={require('common/Enum').imgList.rv_play}
+                          className="icon-inner"
+                          alt=""
+                        />
+                      </span>
+                      <span className="controls-icon">
+                        <img
+                          src={require('common/Enum').imgList.rvAdd}
+                          className="icon-inner"
+                          alt=""
+                        />
+                      </span>
+                      <span className="controls-icon">
+                        <img
+                          src={require('common/Enum').imgList.notDownload}
+                          className="icon-inner"
+                          alt=""
+                        />
+                      </span>
+                      <span className="controls-icon">
+                        <img
+                          src={require('common/Enum').imgList.rvMore}
+                          className="icon-inner"
+                          alt=""
+                        />
+                      </span>
+                    </div>
+                  </div>
+                  <span className="songlist-item--singer one-line-ellipsis">
+                    {song.singer.reduce((acc, cur) => acc + '/' + cur.name, '').slice(1)}
+                  </span>
+                  <span className="songlist-item--album one-line-ellipsis">{song.album.name}</span>
+                  <span className="songlist-item--interval">{formatSeconds(song.interval)}</span>
+                </li>
+              ))}
+            </ul>
+          </TabPane>
+          <TabPane tab="评论" key="comment">
+            评论列表
+          </TabPane>
+        </Tabs>
+      </div>
     </div>
   )
 }
