ab517d2d653522c6882aacad445f63ef19e79713
Zjinxing <1182999456@qq.com>
2020-08-11 09:00:03 +0800

    获取vKey

--- src/request/playlist.ts
+++ src/request/playlist.ts
@@ -1,4 +1,13 @@
 import { Playlist } from './types/Playlist'
 import { lInstance } from './instance'
+import { MusicVkey } from './types/MusicVkey'
 
-export const GET_LIST_DETAIL = (disstid: string): Promise<Playlist> => lInstance.get('getSongListDetail', { params: { disstid } })
+export const GET_LIST_DETAIL = (disstid: string): Promise<Playlist> =>
+  lInstance.get('getSongListDetail', { params: { disstid } })
+
+/**
+ * 获取 vkey
+ * @param songmid 歌曲mid
+ */
+export const GET_VKEY = (songmid: string): Promise<MusicVkey> =>
+  lInstance.get('getMusicVkey', { params: { songmid } })
