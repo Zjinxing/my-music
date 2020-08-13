3bd6398b72105550fe8c2783bec891ab08d839b7
Zjinxing <1182999456@qq.com>
2020-08-13 08:47:04 +0800

    bottom style

--- src/Components/Container/PlayControl/index.tsx
+++ src/Components/Container/PlayControl/index.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, SyntheticEvent } from 'react'
 import { observer } from 'mobx-react'
 import Progress, { TargetInfo, MousePos } from 'components/Common/Progress'
 import { useStore } from 'store'
 import { PlayMode } from 'common/Enum'
 import './index.scss'
+import { formatSeconds } from 'common/utils'
 
 const PlayControl: React.FC = observer(() => {
   const store = useStore()
@@ -11,6 +12,17 @@ const PlayControl: React.FC = observer(() => {
   const [duration, setDuration] = useState(0) // 当前歌曲总时长
   const [percent, setPercent] = useState(0) // 播放进度
   const [isDragging, setIsDragging] = useState(false)
+
+  const audioRef = useRef<HTMLAudioElement>(null)
+
+  const loadedMetaData = (e: SyntheticEvent<HTMLAudioElement, Event>) => {
+    const target = e.target as HTMLAudioElement
+    console.log('准备好播放', target.duration)
+    const duration = target.duration
+    target.play()
+    store.isPlaying = true
+    setDuration(duration)
+  }
 
   const onProgressChange = (targetInfo: TargetInfo, pos: MousePos) => {
     setIsDragging(true)
@@ -20,6 +32,26 @@ const PlayControl: React.FC = observer(() => {
     const percent = compute >= 100 ? 100 : compute <= 0 ? 0 : compute
     setPercent(percent)
   }
+
+  const togglePlay = () => {
+    if (store.isPlaying) {
+      audioRef.current?.pause()
+      store.isPlaying = false
+    } else {
+      audioRef.current?.play()
+      store.isPlaying = true
+    }
+  }
+
+  // 播放更新进度条
+  const onProgress = (e: SyntheticEvent<HTMLAudioElement>) => {
+    const target = e.target as HTMLAudioElement
+    const current = target.currentTime
+    const percent = duration && (current / duration) * 100
+    percent && !isDragging && setPercent(percent)
+    setCurrentTime(current)
+  }
+
   return (
     <div className="play-control">
       <Progress width={percent} onProgressChange={onProgressChange}></Progress>
@@ -50,35 +82,62 @@ const PlayControl: React.FC = observer(() => {
           </span>
         </div>
         <div className="control-content--center">
-          {store.playMode === PlayMode.LOOP ? (
-            <img
-              src={require('common/Enum').imgList.playModeOrder}
-              alt=""
-              className="control-icon "
-            />
-          ) : store.playMode === PlayMode.RANDOM ? (
-            <img
-              src={require('common/Enum').imgList.playModeRandom}
-              alt=""
-              className="control-icon"
-            />
-          ) : (
-            <img
-              src={require('common/Enum').imgList.playModeSingle}
-              alt=""
-              className="control-icon"
-            />
-          )}
-          <img src={require('common/Enum').imgList.preSong} alt="" className="control-icon " />
-          <img src={require('common/Enum').imgList.playSong} alt="" className="control-icon" />
-          <img src={require('common/Enum').imgList.nextSong} alt="" className="control-icon" />
-          <img src={require('common/Enum').imgList.volume} alt="" className="control-icon" />
+          <span className="control-icon">
+            {store.playMode === PlayMode.LOOP ? (
+              <img src={require('common/Enum').imgList.playModeOrder} alt="" />
+            ) : store.playMode === PlayMode.RANDOM ? (
+              <img src={require('common/Enum').imgList.playModeRandom} alt="" />
+            ) : (
+              <img src={require('common/Enum').imgList.playModeSingle} alt="" />
+            )}
+          </span>
+          <span className="control-icon">
+            <img src={require('common/Enum').imgList.preSong} alt="" />
+          </span>
+          <span className="control-icon" onClick={togglePlay}>
+            {store.isPlaying ? (
+              <img src={require('common/Enum').imgList.pauseSong} alt="" />
+            ) : (
+              <img src={require('common/Enum').imgList.playSong} alt="" />
+            )}
+          </span>
+          <span className="control-icon">
+            <img src={require('common/Enum').imgList.nextSong} alt="" />
+          </span>
+          <span className="control-icon">
+            <img src={require('common/Enum').imgList.volume} alt="" />
+          </span>
         </div>
         <div className="control-content--right">
-          <img src={require('common/Enum').imgList.qualityNormal} alt="" />
+          <div className="right-wrapper">
+            <span className="control-icon">
+              <img src={require('common/Enum').imgList.qualityNormal} alt="" width="41" />
+            </span>
+            <span className="control-icon">
+              {store.effect ? (
+                <img src={require('common/Enum').imgList.effectOn} alt="" width="41" />
+              ) : (
+                <img src={require('common/Enum').imgList.effectOff} alt="" width="41" />
+              )}
+            </span>
+          </div>
+          <span className="play-time">
+            {formatSeconds(currentTime)} / {formatSeconds(duration)}
+          </span>
+          <span className="control-icon">
+            <img src={require('common/Enum').imgList.lyric} alt="" />
+          </span>
+          <span className="control-icon">
+            <img src={require('common/Enum').imgList.playlist} alt="" />
+          </span>
         </div>
       </div>
-      <audio src={store.currentSongUrl}></audio>
+      <audio
+        ref={audioRef}
+        src={store.currentSongUrl}
+        onLoadedMetadata={loadedMetaData}
+        onTimeUpdate={onProgress}
+      ></audio>
     </div>
   )
 })
