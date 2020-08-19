b2f7d3ece00f72081a5a743ff662b3680ca831cd
Zjinxing <1182999456@qq.com>
2020-08-19 09:01:45 +0800

    更改store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -3,6 +3,7 @@ import React, { createContext, useContext } from 'react'
 import { PlaylistSong } from 'request/types/Playlist'
 import Recommend, { AlbumDetail } from 'request/types/Recommend'
 import { HotSinger } from 'request/types/Singer'
+import { AlbumSongDetail } from 'request/types/Album'
 
 export interface State {
   recommend: Recommend
@@ -12,8 +13,10 @@ export interface State {
   currentSongUrl: string // 当前播放歌曲地址
   currentSongName: string // 当前播放歌曲的名称
   currentPlaylistId: number // 当前播放列表 id
-  currentSong: PlaylistSong | null // 当前播放歌曲
-  playlist: PlaylistSong[] // 当前播放列表详情
+  currentSong: PlaylistSong | AlbumSongDetail | null // 当前播放歌曲
+  playType: 'playlist' | 'singer' | 'album' // 播放列表类型：歌单，歌手，专辑
+  playlist: PlaylistSong[] // 当前播放列表详情 - 播放歌单
+  playlistAlbum: AlbumSongDetail[] // 当前播放列表详情 - 播放专辑
   albumList: AlbumDetail[] // 首页新专辑
   albumArea: 1 | 2 | 3 | 4 | 5 | 6 // 当前专辑地区
   playMode: 'loop' | 'random' | 'singleLoop' // 播放模式
@@ -34,7 +37,9 @@ export const createStore = (): State => {
     currentSongName: '',
     currentPlaylistId: 0,
     currentSong: null,
+    playType: 'playlist',
     playlist: [],
+    playlistAlbum: [],
     albumList: [],
     albumArea: 1,
     playMode: 'loop',
