69a35cc1cd843713bbac6f1a669b4d8150e5c56a
Zjinxing <1182999456@qq.com>
2020-09-11 08:51:12 +0800

    歌手单曲接口

--- src/request/singer.ts
+++ src/request/singer.ts
@@ -1,7 +1,7 @@
 import { uInstance } from './instance'
 import { commonConfig } from './commonConfig'
 import { generateSign } from 'common/utils'
-import { HotSingerRes, SingerDetail, SimilarSinger } from './types/Singer'
+import { HotSingerRes, SingerDetail, SimilarSinger, GetSingerSong } from './types/Singer'
 import { SingerAlbumList } from './types/Album'
 
 export const GET_SINGERS = (args?: {
@@ -33,7 +33,7 @@ export const GET_SINGERS = (args?: {
   return uInstance.get('cgi-bin/musics.fcg', { params })
 }
 
-export const GET_SINGERSONG = (singerid: string): Promise<SingerDetail> =>
+export const GET_SINGER_DETAIL = (singerid: string): Promise<SingerDetail> =>
   uInstance.get(
     `https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_detail_cp.fcg?newsong=1&tpl=wk&singerid=${singerid}&g_tk=5381&platform=mac&g_tk_new_20200303=5381&loginUin=0&hostUin=0&format=json&inCharset=GB2312&outCharset=utf-8&notice=0&platform=jqspaframe.json&needNewCode=0&ct=6&cv=10000`
   )
@@ -54,4 +54,12 @@ export const GET_SINGER_ALBUM = (singermid: string): Promise<SingerAlbumList> =>
 export const GET_SIM_SINGERS = (singermid: string): Promise<SimilarSinger> =>
   uInstance.get(
     `https://c.y.qq.com/v8/fcg-bin/fcg_v8_simsinger.fcg?utf8=1&singer_mid=${singermid}&start=0&num=5&g_tk_new_20200303=5381&g_tk=5381&loginUin=0&hostUin=0&inCharset=GB2312&outCharset=utf-8&notice=0&platform=mac&needNewCode=0`
+  )
+
+/**
+ * 获取歌手歌曲
+ */
+export const GET_SINGER_SONG = (begin: number = 0, num: number = 30): Promise<GetSingerSong> =>
+  uInstance.get(
+    `https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg?g_tk_new_20200303=5381&g_tk=5381&loginUin=0&hostUin=0&inCharset=GB2312&outCharset=utf-8&notice=0&platform=mac&needNewCode=0&singermid=00067r4p0wBDDN&order=listen&begin=${begin}&num=${num}&songstatus=1&newsong=1`
   )
--- src/request/types/Singer.ts
+++ src/request/types/Singer.ts
@@ -42,102 +42,100 @@ interface Singer extends SingerBase {
 }
 
 export interface SongInfo {
-  songInfo: {
-    action: {
-      switch: number
-      msgid: number
-      alert: number
-      icons: number
-      msgshare: number
-      msgfav: number
-      msgdown: number
-      msgpay: number
-    }
-    aid: number
-    album: {
-      id: number
-      mid: string
-      name: string
-      pmid: string
-      subtitle: string
-      time_public: string
-      title: string
-    }
-    bpm: number
-    data_type: number
-    es: string
-    file: {
-      b_30s: number
-      e_30s: number
-      hires_bitdepth: number
-      hires_sample: number
-      media_mid: string
-      size_24aac: number
-      size_48aac: number
-      size_96aac: number
-      size_192aac: number
-      size_192ogg: number
-      size_128mp3: number
-      size_320mp3: number
-      size_ogg: number
-      size_128: number
-      size_320: number
-      size_ape: number
-      size_flac: number
-      size_dts: number
-      url: string
-    }
-    fnote: number
-    genre: number
+  action: {
+    switch: number
+    msgid: number
+    alert: number
+    icons: number
+    msgshare: number
+    msgfav: number
+    msgdown: number
+    msgpay: number
+  }
+  aid: number
+  album: {
     id: number
-    index_album: number
-    index_cd: number
-    interval: number
-    isonly: number
-    ksong: {
-      id: number
-      mid: string
-    }
-    label: string
-    language: number
     mid: string
-    modify_stamp: number
-    mv: {
-      id: number
-      vid: string
-      name: string
-      title: string
-      vt: number
-    }
     name: string
-    ov: number
-    pay: {
-      pay_month: number
-      price_track: number
-      price_album: number
-      pay_play: number
-      pay_down: number
-      pay_status: number
-      time_free: number
-    }
-    pingpong: string
-    ppurl: string
-    sa: number
-    singer: Singer[]
-    status: number
+    pmid: string
     subtitle: string
-    tid: number
     time_public: string
     title: string
-    trace: string
-    type: number
+  }
+  bpm: number
+  data_type: number
+  es: string
+  file: {
+    b_30s: number
+    e_30s: number
+    hires_bitdepth: number
+    hires_sample: number
+    media_mid: string
+    size_24aac: number
+    size_48aac: number
+    size_96aac: number
+    size_192aac: number
+    size_192ogg: number
+    size_128mp3: number
+    size_320mp3: number
+    size_ogg: number
+    size_128: number
+    size_320: number
+    size_ape: number
+    size_flac: number
+    size_dts: number
     url: string
-    version: number
-    volume: {
-      gain: number
-      peak: number
-      lra: number
-    }
+  }
+  fnote: number
+  genre: number
+  id: number
+  index_album: number
+  index_cd: number
+  interval: number
+  isonly: number
+  ksong: {
+    id: number
+    mid: string
+  }
+  label: string
+  language: number
+  mid: string
+  modify_stamp: number
+  mv: {
+    id: number
+    vid: string
+    name: string
+    title: string
+    vt: number
+  }
+  name: string
+  ov: number
+  pay: {
+    pay_month: number
+    price_track: number
+    price_album: number
+    pay_play: number
+    pay_down: number
+    pay_status: number
+    time_free: number
+  }
+  pingpong: string
+  ppurl: string
+  sa: number
+  singer: Singer[]
+  status: number
+  subtitle: string
+  tid: number
+  time_public: string
+  title: string
+  trace: string
+  type: number
+  url: string
+  version: number
+  volume: {
+    gain: number
+    peak: number
+    lra: number
   }
 }
 
@@ -201,4 +199,29 @@ export interface SimilarSinger {
     hasmore: number
     items: SimSinger[]
   }
+}
+
+export interface GetSingerSong {
+  code: number
+  data: {
+    list: {
+      Flisten_count1: number
+      Fupload_time: string
+      index: number
+      isnew: number
+      listenCount: number
+      musicData: SongInfo
+      playurl: ''
+      price: number
+      vid: {
+        Fstatus: null
+      }
+    }[]
+    singer_id: string
+    singer_mid: string
+    singer_name: string
+    total: number
+  }
+  message: string
+  subcode: number
 }
