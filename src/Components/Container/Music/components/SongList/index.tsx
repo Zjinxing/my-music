8d93308dd0d41180b444ece9f0980cbdc295e280
Zjinxing <1182999456@qq.com>
2020-09-17 09:02:52 +0800

    分类歌单

--- src/Components/Container/Music/components/SongList/index.tsx
+++ src/Components/Container/Music/components/SongList/index.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { GET_PLAYLIST_BY_TAG } from 'request/playlist'
-import { Vitem } from 'request/types/Playlist'
+import { Vitem, Vplaylist } from 'request/types/Playlist'
 import './index.scss'
 
 const SongList: React.FC = () => {
   const [playlistTabs, setPlaylistTabs] = useState<Vitem[]>()
+  const [playlist, setPlaylist] = useState<Vplaylist[]>()
   useEffect(() => {
     ;(async () => {
       const res = await GET_PLAYLIST_BY_TAG()
       console.log(res)
       setPlaylistTabs(res.req_1.data.v_item)
+      setPlaylist(res.req_0.data.v_playlist)
     })()
   }, [])
   return (
@@ -30,7 +32,32 @@ const SongList: React.FC = () => {
             <li>最新</li>
           </ul>
         </div>
-        <ul className="playlist-content-list"></ul>
+        <ul className="playlist-content-list">
+          {playlist?.map(item => (
+            <li className="playlist-content-list-item">
+              <div className="playlist-cover-content">
+                <img
+                  src={item.cover_url_medium}
+                  alt=""
+                  className="playlist-cover-content--bg"
+                  width="100%"
+                />
+                <span className="playlist-cover-content--control">
+                  <img src={require('common/Enum').imgList.play} alt="" />
+                </span>
+                <span className="listeners">
+                  <img
+                    src={require('common/Enum').imgList.listen}
+                    alt=""
+                    width="16"
+                    className="icon"
+                  />
+                  {(item.access_num / 10000).toFixed(1)}万
+                </span>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   )
